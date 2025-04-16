95b8efcf3bdc0cb39849e2422e45e219ec413d9a
Vuong Minh Khoi <trunghien2807@gmail.com>
2025-04-16 08:30:36 +0700

    logic cau 2 phan 1

---
+++ NET_Midterm2025/Controllers/SachesController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NET_Midterm2025.Models;
+using NET_Midterm2025.Models.DTOs;
+
+namespace NET_Midterm2025.Controllers
+{
+    public class SachesController : Controller
+    {
+        private readonly ThuVienContext _context;
+
+        public SachesController(ThuVienContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Saches
+        public async Task<IActionResult> Index()
+        {
+            var thuVienContext = _context.Saches.Include(s => s.MaNxbNavigation);
+            return View(await thuVienContext.ToListAsync());
+        }
+
+        // GET: Saches/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sach = await _context.Saches
+                .Include(s => s.MaNxbNavigation)
+                .FirstOrDefaultAsync(m => m.MaSach == id);
+            if (sach == null)
+            {
+                return NotFound();
+            }
+
+            return View(sach);
+        }
+
+        // GET: Saches/Create
+        public IActionResult Create()
+        {
+            ViewData["MaNxb"] = new SelectList(_context.Nhaxuatbans, "MaNxb", "MaNxb");
+            return View();
+        }
+
+        // POST: Saches/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SachCreateDTO sach)
+        {
+            if (ModelState.IsValid)
+            {
+                var newSach = new Sach
+                {
+                    TenSach = sach.TenSach,
+                    AnhBia = sach.AnhBia,
+                    Isbn = sach.Isbn,
+                    TacGia = sach.TacGia,
+                    NamXuatBan = sach.NamXuatBan,
+                    MaNxb = sach.MaNxb,
+                    TriGia = sach.TriGia,
+                    NgayNhap = sach.NgayNhap
+                };
+                _context.Add(newSach);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaNxb"] = new SelectList(_context.Nhaxuatbans, "MaNxb", "MaNxb", sach.MaNxb);
+            return View(sach);
+        }
+
+        // GET: Saches/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sach = await _context.Saches.FindAsync(id);
+            if (sach == null)
+            {
+                return NotFound();
+            }
+            ViewData["MaNxb"] = new SelectList(_context.Nhaxuatbans, "MaNxb", "MaNxb", sach.MaNxb);
+            return View(sach);
+        }
+
+        // POST: Saches/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MaSach,TenSach,AnhBia,Isbn,TacGia,NamXuatBan,MaNxb,TriGia,NgayNhap")] Sach sach)
+        {
+            if (id != sach.MaSach)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    sach.NgayNhap = DateTime.UtcNow;
+                    _context.Update(sach);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SachExists(sach.MaSach))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaNxb"] = new SelectList(_context.Nhaxuatbans, "MaNxb", "MaNxb", sach.MaNxb);
+            return View(sach);
+        }
+
+        // GET: Saches/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sach = await _context.Saches
+                .Include(s => s.MaNxbNavigation)
+                .FirstOrDefaultAsync(m => m.MaSach == id);
+            if (sach == null)
+            {
+                return NotFound();
+            }
+
+            return View(sach);
+        }
+
+        // POST: Saches/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var sach = await _context.Saches.FindAsync(id);
+            if (sach != null)
+            {
+                _context.Saches.Remove(sach);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SachExists(int id)
+        {
+            return _context.Saches.Any(e => e.MaSach == id);
+        }
+    }
+}
---
+++ NET_Midterm2025/Models/DTOs/SachCreateDTO.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace NET_Midterm2025.Models.DTOs
+{
+    public class SachCreateDTO
+    {
+        [Display(Name = "Tên sách")]
+        public string TenSach { get; set; } = null!;
+        [Display(Name = "Ảnh bìa sách")]
+        [DataType(DataType.Text)]
+        public string AnhBia { get; set; }
+        [Display(Name = "ISBN")]
+        public string? Isbn { get; set; }
+        [Display(Name = "Tác giả")]
+        public string? TacGia { get; set; }
+        [Display(Name = "Năm xuất bản")]
+        public int? NamXuatBan { get; set; }
+        [Display(Name = "Nhà xuất bản")]
+        public string? MaNxb { get; set; }
+        [Display(Name = "Giá trị")]
+        public double TriGia { get; set; }
+        [Display(Name = "Ngày nhập")]
+        public DateTime NgayNhap = DateTime.UtcNow;
+    }
+}
